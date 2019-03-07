14b51d389bdf7b84ea1f41ea1fb262d8b861747c
Andrew Vuong <anvuong3@s.sfusd.edu>
2019-03-06 17:32:44 -0800

    mad-libs

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
-document.getElementById("name").innerText = window.prompt("What is your name?");
+function startMadLibs() {
+    document.getElementById("name").innerText = window.prompt("What is your name?");
 
-let noun = window.prompt("Welcome to a Mad-Libs generator, give me a noun");
-document.getElementById("noun1").innerText = noun
 
-document.getElementById("placeLocation1").innerText = window.prompt("Give me a location or place");
 
-let she = window.prompt("Is your creation's proverb he or she?");
-document.getElementById("SHe").innerText = she
+    let noun11 = window.prompt("Welcome to a Mad-Libs generator, give me a noun");
+    document.getElementById("noun1").innerText = noun11;
 
-document.getElementById("adj1").innerText = window.prompt("Give me an adjective");
 
-document.getElementById("adj2").innerText = window.prompt("Give me another adjective please");
+    document.getElementById("placeLocation1").innerText = window.prompt("Give me a location or place");
 
-document.getElementById("quantifiers").innerText = window.prompt("Give me a quantifier, for example a lot, many, few, etc.");
+    let she1 = window.prompt("Is your creation's proverb he or she?");
+    let she = document.querySelectorAll(".SHe");
+    for (let i = 0; i < she.length; i++) {
+        she[i].innerText = she1;
+    }
 
-document.getElementById("listOfFriends").innerText = window.prompt("Give me a list of names, it can be one or two, but remember to put a comma after each name!");
 
-document.getElementById("adj3").innerText = window.prompt("Give me an adjective");
+    document.getElementById("adj1").innerText = window.prompt("Give me an adjective");
 
-document.getElementById("pet").innerText = window.prompt("Give me a pet");
+    document.getElementById("adj2").innerText = window.prompt("Give me another adjective please");
 
-document.getElementById("adj4").innerText = window.prompt("Give me an adjective");
+    document.getElementById("quantifiers").innerText = window.prompt("Give me a quantifier, for example a lot, many, few, etc.");
 
-document.getElementById("verb").innerText = window.prompt("Give me a verb");
+    document.getElementById("listOfFriends").innerText = window.prompt("Give me a list of names, it can be one or two, but remember to put a comma after each name!");
 
-let noun2= window.prompt("Give me a noun");
-document.getElementById("noun2").innerText = noun2
+    document.getElementById("adj3").innerText = window.prompt("Give me an adjective");
 
-let verb2 = window.prompt("Give me a verb");
-document.getElementById("verb2").innerText = verb2
 
-let noun3 = window.prompt("Give me a noun");
-document.getElementById("noun3").innerText = noun3
+    let pet1 = window.prompt("Give me a pet");
+    document.getElementById("pet").innerText = pet1
 
-document.getElementById("hisHer").innerText = window.prompt("Does your creation go by his or her?");
 
-let verb3 = window.prompt("Give me a verb");
-document.getElementById("verb3").innerText = verb3
+
+    let adj41 = window.prompt("Give me an adjective");
+    document.getElementById("adj4").innerText = adj41
+
+
+
+    let verb11 = window.prompt("Give me a verb");
+    document.getElementById("verb").innerText = verb11
+
+
+
+    let noun21= window.prompt("Give me a noun");
+    document.getElementById("noun2").innerText = noun21;
+
+    let verb21 = window.prompt("Give me a verb");
+    document.getElementById("verb2").innerText = verb21;
+
+
+    let noun31 = window.prompt("Give me a noun");
+    document.getElementById("noun3").innerText = noun31;
+
+
+    let hisHer1 = window.prompt("Does your creation go by his or her?");
+    document.getElementById("hisHer").innerText = hisHer1
+
+
+
+    let verb3 = window.prompt("Give me a verb");
+    document.getElementById("verb3").innerText = verb3;
+}
+
+
+
+document.getElementById('StartMadLibs').onclick = startMadLibs;
