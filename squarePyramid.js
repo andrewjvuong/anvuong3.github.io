985c3c95eb39bbf5b42e15217ebc5dbfd97c0e5e
Andrew Vuong <anvuong3@s.sfusd.edu>
2019-04-01 17:28:58 -0700

    block

---
+++ squarePyramid.js
@@ -0,0 +1,30 @@
+const canvasWidth = 700;
+const canvasHeight = 500;
+const blockWidth = 50;
+const blockHeight = 25;
+const rowNumb = 5;
+const blockNumb = 10
+
+let draw = SVG('canvas').size(canvasWidth, canvasHeight);
+
+
+const WHITE = '#fff';
+const BLACK = '#000';
+
+for (let i = 0; i < rowNumb; i++){
+    let rowY = i * blockHeight;
+
+    for(let j = 0; j < blockNumb; j++) {
+        let blockX = j * blockWidth;
+
+        let rect = {
+            element: draw.rect(blockWidth, blockHeight),
+            color: WHITE
+        };
+
+        rect.element.move(blockX, rowY);
+        rect.element.stroke(BLACK);
+        rect.element.fill('blanchedAlmond')
+
+    }
+}
